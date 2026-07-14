6ed0c0dec2c059c94664704af43d5930d4189056
Jhoam Garcia <jhoamgb@gmail.com>
2026-07-13 23:24:06 -0400

    Make DOC EASYCAR installable

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'doc-easycar-shell-v1';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/easycar-docuseal-logo-400.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (
+    event.request.method !== 'GET'
+    || url.origin !== self.location.origin
+    || url.pathname.startsWith('/api/')
+  ) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('/')));
+  }
+});
